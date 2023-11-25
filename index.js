cbbfd6f0a5e3d4a02161b6d6c53cb653bf2b178c
Mohammad <mo7amed.2197@gmail.com>
2023-11-25 10:07:09 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare customerName to be 'bob' in the global scope
+var customerName = 'bob';
+
+// Function to uppercase the customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set the bestCustomer variable
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite the bestCustomer variable
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+// Constant variable for the least favorite customer
+const leastFavoriteCustomer = 'someone';
+
+// Function to try to reassign the least favorite customer (throws an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
